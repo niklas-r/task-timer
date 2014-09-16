d8bf8d0b9200cc129143a0b799e3b16dccfcc33d
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2014-09-16 19:44:29 +0200

    add es5-shim

--- build.config.js
+++ build.config.js
@@ -59,8 +59,9 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/es5-shim/es5-shim.js',
       'vendor/angular/angular.js',
-      'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
+      'vendor/angular-bootstrap/ui-bootstrap-tpls.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js'
     ],
