90e88ce335ccf206d5af70d47fb114a70db7e149
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2014-11-11 19:54:54 +0100

    add timer model

--- src/app/clock/clock.js
+++ src/app/clock/clock.js
@@ -7,10 +7,11 @@
   /**
    * A clock factory from which new clocks can be instantiated
    * @param {angular.$rootScope} $rootScope
+   * @param {timer} timer
    * @ngInject
    * @return {clock}
    */
-  function clockFactory ($rootScope) {
+  function clockFactory ($rootScope, timer) {
     /**
      * The clock settings object
      * @typedef {object} clockSettings
@@ -52,7 +53,7 @@
        * Tracked time
        * @type {number}
        */
-      clock.time = 0;
+      clock.time = timer.create();
 
       /**
        * Clock state
@@ -88,16 +89,17 @@
       /**
        * Tick time
        * @param {number} milliseconds Amount of time in milliseconds to be added
-       *                              to clock.time.
+       *                              to clock.time.ms
        */
       clock.tick = function (milliseconds) {
 
         if ( clock.countUp ) {
-          clock.time += milliseconds;
+          clock.time.ms += milliseconds;
         } else {
-          clock.time -= milliseconds;
+          clock.time.ms -= milliseconds;
         }
 
+        // TODO: Replace this with $evalAsync because checking $$phase is hacky
         if ( !$rootScope.$$phase ) {
           $rootScope.$apply();
         }
@@ -135,6 +137,143 @@
     };
   }
 
+  /**
+   * Timer factory
+   * @return {timer}
+   */
+  function timerFactory () {
+    var api,
+        _delimiter,
+        MS_IN_ONE_HOUR,
+        MS_IN_ONE_MINUTE,
+        MS_IN_ONE_SECOND;
+
+    /** @constant {number} */
+    MS_IN_ONE_SECOND = 1000;
+    /** @constant {number} */
+    MS_IN_ONE_MINUTE = MS_IN_ONE_SECOND * 60;
+    /** @constant {number} */
+    MS_IN_ONE_HOUR = MS_IN_ONE_MINUTE * 60;
+
+    _delimiter = ':';
+
+    api = {};
+
+    function updateTimeProperty(key, newValue, msConstant, time) {
+      var oldValue,
+          oldValueInMs,
+          newValueInMs;
+
+      // Store old value
+      oldValue = time[key];
+      newValue = parseInt(newValue, 10);
+
+      // Convert values to ms
+      oldValueInMs = oldValue * msConstant;
+      newValueInMs = newValue * msConstant;
+
+      // Update properties
+      time.ms = time.ms - oldValueInMs + newValueInMs;
+      time[key] = newValue;
+    }
+
+    function padNum (num) {
+      if ( num < 10) {
+        return '0' + num;
+      } else {
+        return num;
+      }
+    }
+
+    api.create = function () {
+      var timeObject,
+          _time;
+
+      _time = {
+        __constructor__: 'timer',
+        hr: 0,
+        min: 0,
+        sec: 0,
+        ms: 0
+      };
+
+      /**
+       * Timer object
+       * @typedef {object} timer
+       * @property {number} hr  Tracked hours
+       * @property {number} min Tracked minuters
+       * @property {number} sec Tracked seconds
+       * @property {number} ms  Tracked milliseconds
+       * @function toString Returns a formated string of the tracked time
+       */
+      timeObject = {
+        //////////
+        // Hour //
+        //////////
+        get hr() {
+          return _time.hr;
+        },
+        set hr(newHr) {
+          updateTimeProperty('hr', newHr, MS_IN_ONE_HOUR, _time);
+        },
+        ////////////
+        // Minute //
+        ////////////
+        get min() {
+          return _time.min;
+        },
+        set min(newMin) {
+          updateTimeProperty('min', newMin, MS_IN_ONE_MINUTE, _time);
+        },
+        ////////////
+        // Second //
+        ////////////
+        get sec() {
+          return _time.sec;
+        },
+        set sec(newSec) {
+          updateTimeProperty('sec', newSec, MS_IN_ONE_SECOND, _time);
+        },
+        /////////////////
+        // Millisecond //
+        /////////////////
+        get ms() {
+          return _time.ms;
+        },
+        set ms(newMs) {
+          _time.ms = newMs;
+
+          // Convert tracked milliseconds to hours
+          _time.hr = Math.floor(newMs / MS_IN_ONE_HOUR);
+          // Subtract tracked hours (in ms) from newly added ms
+          newMs -= ( _time.hr * MS_IN_ONE_HOUR );
+
+          // Continue to convert and set whole minutes
+          _time.min = Math.floor(newMs / MS_IN_ONE_MINUTE);
+          // Subtract tracked minutes (in ms)
+          newMs -= ( _time.min * MS_IN_ONE_MINUTE );
+
+          // Finally convert rest of milliseconds to whole seconds
+          _time.sec = Math.floor(newMs / MS_IN_ONE_SECOND);
+        },
+        toString: function () {
+          return (
+            padNum(_time.hr) +
+            _delimiter +
+            padNum(_time.min) +
+            _delimiter +
+            padNum(_time.sec)
+          );
+        }
+      };
+
+      return Object.seal(timeObject);
+    };
+
+    return api;
+  }
+
   clockModule.factory('clock', clockFactory);
+  clockModule.factory('timer', timerFactory);
 
 }(angular));
--- src/app/clock/clock.spec.js
+++ src/app/clock/clock.spec.js
@@ -5,9 +5,145 @@ describe('module: taskTimer.clock', function () {
 
   beforeEach(module('taskTimer.clock'));
 
+  describe.only('class: Time', function () {
+
+    it('it should have properties representing time', inject(
+      function (timer) {
+        var time = timer.create();
+
+        expect(time.hr).to.equal(0);
+        expect(time.min).to.equal(0);
+        expect(time.sec).to.equal(0);
+        expect(time.ms).to.equal(0);
+
+      })
+    );
+
+    it('should provide a string in the format of HH:MM:SS', inject(
+      function (timer) {
+        var time = timer.create();
+
+        expect(time.toString()).to.equal('00:00:00');
+
+        time.hr = 4;
+        time.min = 32;
+        time.sec = 8;
+
+        expect(time.toString()).to.equal('04:32:08');
+      })
+    );
+
+    it('should return new objects on each function call', inject(
+      function (timer) {
+        var t1, t2;
+
+        t1 = timer.create();
+        t2 = timer.create();
+
+        expect(t1).not.to.equal(t2);
+      })
+    );
+
+    describe('when updating', function () {
+
+      describe('hours', function () {
+
+        it('should auto update ms', inject(
+          function (timer) {
+            var time = timer.create();
+
+            time.hr = 5;
+
+            expect(time.hr).to.equal(5);
+            expect(time.ms).to.equal(18000000);
+
+          })
+        );
+
+      });
+
+      describe('minutes', function () {
+
+        it('should auto update ms', inject(
+          function (timer) {
+            var time = timer.create();
+
+            time.min = 4;
+
+            expect(time.min).to.equal(4);
+            expect(time.ms).to.equal(240000);
+
+          })
+        );
+
+      });
+
+      describe('seconds', function () {
+
+        it('should auto update ms', inject(
+          function (timer) {
+            var time = timer.create();
+
+            time.sec = 45;
+
+            expect(time.sec).to.equal(45);
+            expect(time.ms).to.equal(45000);
+
+          })
+        );
+
+      });
+
+      describe('milliseconds', function () {
+
+        it('should auto update hours, minutes and seconds', inject(
+          function (timer) {
+            var time = timer.create();
+
+            time.ms = 50972000;
+
+            // (1000) * 32 + (1000 * 60) * 9 + (1000 * 60 * 60) * 14 = 50972000
+
+            expect(time.sec).to.equal(32);
+            expect(time.min).to.equal(9);
+            expect(time.hr).to.equal(14);
+            expect(time.ms).to.equal(50972000);
+
+          })
+        );
+
+      });
+
+      describe('mixed properties', function () {
+        it('it should work together', inject(
+          function (timer) {
+            var time = timer.create();
+
+            time.sec = 45;
+            time.min = 50;
+            time.hr = 2;
+
+            expect(time.sec).to.equal(45);
+            expect(time.min).to.equal(50);
+            expect(time.hr).to.equal(2);
+            expect(time.ms).to.equal(10245000);
+
+          })
+        );
+      });
+
+    });
+
+  });
+
   describe('object: clock', function () {
+    var timeObjectStub;
 
     function setupClock(clock, settings) {
+      timeObjectStub.returns({
+        ms: 0
+      });
+
       return clock(settings);
     }
 
@@ -25,9 +161,21 @@ describe('module: taskTimer.clock', function () {
       });
     }
 
+    function provideTimeObjectMock ($provide) {
+      timeObjectStub = sinon.stub();
+
+      $provide.value('timeObject', timeObjectStub);
+    }
+
     describe('properties', function () {
-      it('should have a label', inject(
-        function (_clock_) {
+
+      it('should have a label', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
+        inject(function (_clock_) {
           var clockIncrease,
               clockDecrease;
 
@@ -36,11 +184,16 @@ describe('module: taskTimer.clock', function () {
 
           expect(clockIncrease).to.have.property('label', 'Count up clock');
           expect(clockDecrease).to.have.property('label', 'Count down clock');
-        })
-      );
+        });
 
-      it(
-        'should be able to count up and down',
+      });
+
+      it('should be able to count up and down', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
         inject( function (_clock_) {
           var clockIncrease,
               clockDecrease;
@@ -50,28 +203,55 @@ describe('module: taskTimer.clock', function () {
 
           expect(clockIncrease).to.have.property('countUp', true);
           expect(clockDecrease).to.have.property('countUp', false);
-        })
-      );
+        });
+      });
+
+      it('should use timeObject to track time', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
+        inject(function (_clock_) {
+          var clock;
+
+          clock = setupClockIncrease(_clock_);
+
+          expect(clock.time).to.eql({
+            ms: 0
+          });
+        });
+      });
     });
 
     describe('actions', function () {
 
-      it('should tick time', inject( function (_clock_) {
-        var clockIncrease,
-            clockDecrease;
+      it('should tick time', function () {
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
 
-        clockIncrease = setupClockIncrease(_clock_);
-        clockDecrease = setupClockDecrease(_clock_);
+        inject( function (_clock_) {
+          var clockIncrease,
+              clockDecrease;
 
-        clockIncrease.tick(143);
-        clockDecrease.tick(143);
+          clockIncrease = setupClockIncrease(_clock_);
+          clockDecrease = setupClockDecrease(_clock_);
 
-        expect(clockIncrease.time).to.equal(143);
-        expect(clockDecrease.time).to.equal(-143);
-      }));
+          clockIncrease.tick(143);
+          clockDecrease.tick(143);
 
-      it(
-        'should call $apply on each tick',
+          expect(clockIncrease.time.ms).to.equal(143);
+          expect(clockDecrease.time.ms).to.equal(-143);
+        });
+      });
+
+      it('should call $apply on each tick', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
         inject(function ($rootScope, _clock_) {
           var clock;
 
@@ -81,11 +261,16 @@ describe('module: taskTimer.clock', function () {
           clock.tick();
 
           expect($rootScope.$apply).to.have.been.calledOnce;
-        })
-      );
+        });
 
-      it(
-        'should start ticking the time',
+      });
+
+      it('should start ticking the time', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
         inject(function (_clock_) {
           var clock;
           this.clock = sinon.useFakeTimers();
@@ -93,20 +278,25 @@ describe('module: taskTimer.clock', function () {
           clock = setupClockIncrease(_clock_);
 
           clock.start();
-          expect(clock.time).to.equal(0);
+          expect(clock.time.ms).to.equal(0);
 
           this.clock.tick(200);
-          expect(clock.time).to.equal(200);
+          expect(clock.time.ms).to.equal(200);
 
           this.clock.tick(200);
-          expect(clock.time).to.equal(400);
+          expect(clock.time.ms).to.equal(400);
 
           this.clock.restore();
-        })
-      );
+        });
+      });
 
-      it('should pause ticking a started clock', inject(
-        function (_clock_) {
+      it('should pause ticking a started clock', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
+        inject(function (_clock_) {
           var clock;
 
           clock = setupClockIncrease(_clock_);
@@ -116,18 +306,23 @@ describe('module: taskTimer.clock', function () {
           this.clock.tick(554);
           clock.pause();
 
-          expect(clock.time).to.equal(554);
+          expect(clock.time.ms).to.equal(554);
 
           this.clock.tick(100);
 
-          expect(clock.time).to.equal(554);
+          expect(clock.time.ms).to.equal(554);
 
           this.clock.restore();
-        })
-      );
+        });
+      });
 
-      it('should unpause a paused clock', inject(
-        function (_clock_) {
+      it('should unpause a paused clock', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
+        inject(function (_clock_) {
           var clock;
 
           this.clock = sinon.useFakeTimers();
@@ -136,43 +331,54 @@ describe('module: taskTimer.clock', function () {
           // Start clock
           clock.start();
           // Should be zero on init
-          expect(clock.time).to.equal(0);
+          expect(clock.time.ms).to.equal(0);
           // Tick some time
           this.clock.tick(554);
           // On pause it should always update time
           clock.pause();
           // So expect it
-          expect(clock.time).to.equal(554);
+          expect(clock.time.ms).to.equal(554);
           // Time should not be added when paused and ticking
           this.clock.tick(600);
           // Expect it
-          expect(clock.time).to.equal(554);
+          expect(clock.time.ms).to.equal(554);
           // Unpause
           clock.unpause();
           // Now time should be added
           this.clock.tick(200);
           // Expect it
-          expect(clock.time).to.equal(754);
+          expect(clock.time.ms).to.equal(754);
 
           this.clock.restore();
-        })
-      );
+        });
+      });
 
     });
 
     describe('states', function () {
 
-      it('should have stopped state', inject(
-        function (_clock_) {
+      it('should have stopped state', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
+        inject(function (_clock_) {
           var clock;
 
           clock = setupClockIncrease(_clock_);
           expect(clock.state).to.equal('stopped');
-        })
-      );
+        });
 
-      it('should have ticking state', inject(
-        function (_clock_) {
+      });
+
+      it('should have ticking state', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
+        inject(function (_clock_) {
           var clock;
 
           clock = setupClockIncrease(_clock_);
@@ -180,11 +386,17 @@ describe('module: taskTimer.clock', function () {
           clock.start();
 
           expect(clock.state).to.equal('ticking');
-        })
-      );
+        });
 
-      it('should have paused state', inject(
-        function (_clock_) {
+      });
+
+      it('should have paused state', function () {
+
+        module(function ($provide) {
+          provideTimeObjectMock($provide);
+        });
+
+        inject(function (_clock_) {
           var clock;
 
           clock = setupClockIncrease(_clock_);
@@ -193,8 +405,9 @@ describe('module: taskTimer.clock', function () {
           clock.pause();
 
           expect(clock.state).to.equal('paused');
-        })
-      );
+        });
+
+      });
 
     });
 
